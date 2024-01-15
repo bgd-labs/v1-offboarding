630a5ee53c282bd34e3d3687b7a454c1d09135b7
sakulstra <lukasstrassel@googlemail.com>
2024-01-15 12:51:57 +0100

    fix: add deploy script

--- scripts/Deploy.s.sol
+++ scripts/Deploy.s.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.8.0;
 
 import {Script} from 'forge-std/Script.sol';
 
+interface ILendingPool {
+  function initialize(address _addressesProvider) external;
+}
+
 contract Deploy is Script {
+  address constant ADDRESSES_PROVIDER = 0x24a42fD28C976A61Df5D00D0599C34c4f90748c8;
+
   function run() external {
     vm.startBroadcast();
     bytes memory liquidationManagerBytecode = abi.encodePacked(
@@ -24,6 +30,7 @@ contract Deploy is Script {
     assembly {
       poolImpl := create(0, add(lendingPoolBytecode, 0x20), mload(lendingPoolBytecode))
     }
+    ILendingPool(poolImpl).initialize(ADDRESSES_PROVIDER);
     vm.stopBroadcast();
   }
 }
